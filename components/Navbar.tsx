41f8f25161deb6d5ad70be089cb5b1a23ca10558
Andrii Danyliuk <andrij.danyliuk@gmail.com>
2023-09-09 21:59:39 +0200

    navbar and sidebar added

---
+++ components/Navbar.tsx
@@ -0,0 +1,24 @@
+export default function Navbar() {
+  return (
+    <nav className="flex justify-around items-center p-8 h-14 bg-slate-800">
+      <h1>Cargo Planner</h1>
+      <div className="w-2/4">
+        <input
+          className="p-1.5 px-3 rounded-2xl w-full"
+          type="text"
+          aria-label="Search"
+          id="search"
+          placeholder="Search"
+        />
+      </div>
+      <div className="flex justify-end w-1/4">
+        <button className="mx-2 p-1.5 px-4 border-2 rounded-2xl border-rose-500">
+          Load
+        </button>
+        <button className="mx-2 p-1.5 px-4 border-2 rounded-2xl border-rose-500">
+          Save
+        </button>
+      </div>
+    </nav>
+  );
+}
---
+++ components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import CompaniesList from './CompaniesList';
+
+export default function Sidebar() {
+  return (
+    <div className="flex flex-column w-96 border-2 border-green-500 bg-teal-800 p-6">
+      Sidebar
+    </div>
+  );
+}
