aace82c9b3cb193f7f6306fb4f7e8cfdf82b7ce1
Andrii Danyliuk <andrij.danyliuk@gmail.com>
2023-09-10 04:34:05 +0200

    fetching data function added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,32 @@
 'use client';
 
-import { useSelector } from 'react-redux';
-import { selectCompaniesValue } from '@/redux/features/companiesSlice';
+import { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  selectCompaniesValue,
+  setCompanies,
+} from '@/redux/features/companiesSlice';
 
 export default function Navbar() {
   const companies = useSelector(selectCompaniesValue);
+  
+  const dispatch = useDispatch();
 
   const handleSaveClick = () => {
     localStorage.setItem('companies', JSON.stringify(companies));
   };
+
+  const getCompaniesData = useCallback(async () => {
+    try {
+      const response = await fetch('http://localhost:4000/shipments');
+
+      const result = await response.json();
+
+      dispatch(setCompanies(result));
+    } catch (err) {
+      console.error(err);
+    }
+  }, [dispatch]);
 
   return (
     <nav className="flex justify-around items-center p-8 h-14 bg-slate-800">
@@ -23,7 +41,10 @@ export default function Navbar() {
         />
       </div>
       <div className="flex justify-end w-1/4">
-        <button className="mx-2 p-1.5 px-4 border-2 rounded-2xl border-rose-500">
+        <button
+          onClick={getCompaniesData}
+          className="mx-2 p-1.5 px-4 border-2 rounded-2xl border-rose-500"
+        >
           Load
         </button>
         <button
