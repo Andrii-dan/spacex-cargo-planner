a0df813bfc89baafc3f5bc2011289f4a251e82bd
Andrii Danyliuk <andrij.danyliuk@gmail.com>
2023-09-10 15:18:30 +0200

    localStorage bug fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,14 @@
 'use client';
 
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectCompaniesValue, setCompanies } from '@/redux/features/companiesSlice';
+import { useSelector } from 'react-redux';
+import { selectCompaniesValue } from '@/redux/features/companiesSlice';
 
 export default function Home() {
-  const dispatch = useDispatch();
-
   const companies = useSelector(selectCompaniesValue);
 
   const welcomeMessage = companies.length
     ? 'Get started by picking a company from the left sidebar.'
     : "It seems you don't have any locally saved shipments yet. To load your shipments from the network, simply click the 'Load' button.";
-
-  useEffect(() => {
-    const storedData = localStorage.getItem('companies');
-    if (storedData) {
-      const parsedData = JSON.parse(storedData);
-      dispatch(setCompanies(parsedData));
-    }
-  }, [dispatch]);
 
   return (
     <main className="flex flex-col w-full justify-center items-center p-6 border-2 bg-sky-900">
--- components/CompaniesList.tsx
+++ components/CompaniesList.tsx
@@ -1,13 +1,27 @@
 'use client';
 
 import Link from 'next/link';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { selectCompaniesValue } from '@/redux/features/companiesSlice';
+import {
+  selectCompaniesValue,
+  setCompanies,
+} from '@/redux/features/companiesSlice';
 import { Company } from '@/models/company';
+import { useEffect } from 'react';
 
 export default function CompaniesList() {
   const companies = useSelector(selectCompaniesValue);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const storedData = localStorage.getItem('companies');
+    if (storedData) {
+      const parsedData = JSON.parse(storedData);
+      dispatch(setCompanies(parsedData));
+    }
+  }, [dispatch]);
 
   return (
     <>
