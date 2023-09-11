33a19810528976a23efa3ad6fffdd09066eb6403
Andrii Danyliuk <andrij.danyliuk@gmail.com>
2023-09-11 03:34:59 +0200

    home page updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
-'use client';
-
-import { useSelector } from 'react-redux';
-import { selectCompaniesValue } from '@/redux/features/companiesSlice';
-
 export default function Home() {
-  const companies = useSelector(selectCompaniesValue);
-
-  const welcomeMessage = companies.length
-    ? 'Get started by picking a company from the left sidebar.'
-    : "It seems you don't have any locally saved shipments yet. To load your shipments from the network, simply click the 'Load' button.";
-
   return (
-    <main className="flex flex-col w-full justify-center items-center p-6 border-2 bg-sky-900">
-      <h2 className="w-1/3 text-center text-xl">{welcomeMessage}</h2>
+    <main className="flex flex-col w-full justify-center items-center m-4 mx-0 rounded-lg p-6 ">
+      <h2 className="w-1/3 text-center text-xl">
+        {'Get started by picking a company from the left sidebar.'}
+      </h2>
     </main>
   );
 }
