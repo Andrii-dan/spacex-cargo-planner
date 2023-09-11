f3f7a7a0eed1968b0e80613b71e11b3b487ed82f
Andrii Danyliuk <andrij.danyliuk@gmail.com>
2023-09-11 03:32:00 +0200

    loading component added

---
+++ components/LoadingMessage.tsx
@@ -0,0 +1,7 @@
+export default function LoadingMessage() {
+  return (
+    <div className="flex h-full w-full justify-center items-center">
+      <p className="text-gray-700">Loading...</p>
+    </div>
+  );
+}
