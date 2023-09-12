693c1eb8030f3028fb6f9c41f22c637ae7d8c843
Andrii Danyliuk <andrij.danyliuk@gmail.com>
2023-09-12 14:02:15 +0200

    code refactoring

---
+++ components/shared/Snackbar.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { useEffect } from 'react';
+
+type Props = {
+  message: string;
+  isOpen: boolean;
+  onClose: () => void;
+  duration: number;
+};
+
+export default function Snackbar({
+  message,
+  isOpen,
+  onClose,
+  duration,
+}: Props) {
+  useEffect(() => {
+    if (message) {
+      const timer = setTimeout(() => {
+        onClose();
+      }, duration);
+      return () => clearTimeout(timer);
+    }
+  }, [message, onClose, duration]);
+
+  return isOpen ? (
+    <div className='fixed bottom-20 left-2/4 -translate-x-2/4 py-3 px-12 rounded-md bg-primary shadow-lg shadow-slate-600'>
+      <p>{message}</p>
+    </div>
+  ) : null;
+}
